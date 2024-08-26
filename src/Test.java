49cb7f642e483f128ad365729c86ed1564d417a4
bardlinga <balin6546@oslomet.no>
2024-08-26 15:33:10 +0200

    done tom 1.2.5

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,11 @@ import hjelpeklasser.*;
 
 public class Test {
     public static void main(String [] args){
+        int [] tabell = Tabell.randPerm(20);
+        for (int i : tabell) System.out.print(i + " ");
 
+        int maks = Tabell.maks(tabell);
 
-
+        System.out.println("Største verdi har indeks " + maks);
     }
 }
--- src/hjelpeklasser/Tabell.java
+++ src/hjelpeklasser/Tabell.java
@@ -7,11 +7,21 @@ public class Tabell {
 
     private Tabell() {}
 
-    public static void bytt(int[] tabell, int k, int i){
-        int temp = tabell[k];
-        tabell[k] = tabell[i];
-        tabell[i] = temp;
+    // bytt -------------------------------------------------------------------
+
+    public static void bytt(int[] tabell, int i, int j){
+        int temp = tabell[i];
+        tabell[i] = tabell[j];
+        tabell[j] = temp;
     }
+
+    public static void bytt(char[] c, int i, int j){
+        char temp = c[i];
+        c[i] = c[j];
+        c[j] = temp;
+    }
+
+    // permutasjonar ----------------------------------------------------------
 
     public static int[] randPerm(int n)  // en effektiv versjon
     {
@@ -38,6 +48,8 @@ public class Tabell {
         }
     }
 
+    // maks -------------------------------------------------------------------
+
     public static int maks(int[] a, int fra, int til) {
         if (fra < 0 || til > a.length || fra >= til) {
             throw new IllegalArgumentException("Ugyldig intervall!");
@@ -61,6 +73,8 @@ public class Tabell {
         return maks(a,0,a.length);     // kaller metoden over
     }
 
+    // min --------------------------------------------------------------------
+
     public static int min(int[] a, int fra, int til) {
         if (fra < 0 || til > a.length || fra >= til) {
             throw new IllegalArgumentException("Ugyldig intervall!");
@@ -82,5 +96,69 @@ public class Tabell {
 
     public static int min(int[] a){
         return min(a,0,a.length);     // kaller metoden over
+    }
+
+    // nestMaks og nestMin ----------------------------------------------------
+
+    public static int[] nestMaks(int[] tabell) {
+        int n = tabell.length;
+        if (n < 2){
+            throw new java.util.NoSuchElementException("For kort tabell");
+        }
+
+        int maks = 0;
+        int nestMaks = 1;
+        if(tabell[1] > tabell[0]){
+            maks = 1;
+            nestMaks = 0;
+        }
+        int maksverdi = tabell[maks];
+        int nestMaksverdi = tabell[nestMaks];
+
+        for(int i = 2; i < n; i++){
+            if(tabell[i]>nestMaksverdi){
+                if(tabell[i]>maksverdi){
+                    nestMaks = maks;
+                    nestMaksverdi = maksverdi;
+                    maks = i;
+                    maksverdi = tabell[i];
+                } else {
+                    nestMaks = i;
+                    nestMaksverdi = tabell[i];
+                }
+            }
+        }
+        return new int [] {maks, nestMaks};
+    }
+
+    public static int[] nestMin(int[] tabell){
+        int n = tabell.length;
+        if (n < 2){
+            throw new java.util.NoSuchElementException("For kort tabell");
+        }
+
+        int min = 0;
+        int nestMin = 1;
+        if (tabell[1] < tabell[0]){
+            min = 1;
+            nestMin = 0;
+        }
+        int minverdi = tabell[min];
+        int nestMinverdi = tabell[nestMin];
+
+        for (int i = 2; i < n; i++) {
+            if (tabell[i] < nestMinverdi){
+                if (tabell[i] < minverdi){
+                    nestMin = min;
+                    nestMinverdi = minverdi;
+                    min = i;
+                    minverdi = tabell[i];
+                } else {
+                    nestMin = i;
+                    nestMinverdi = tabell[i];
+                }
+            }
+        }
+        return new int[] {min, nestMin};
     }
 }
