ae8efeab89e84e9b7c9c3ee04313bd580b8ad65e
bardlinga <balin6546@oslomet.no>
2024-09-18 12:01:14 +0200

    meh

--- src/Test.java
+++ src/Test.java
@@ -2,14 +2,6 @@ import hjelpeklasser.*;
 import java.util.*;
 
 public class Test {
-    public static void main(String [] args){
-        quicksortTest();
-    }
-
-    private static void quicksortTest(){
-        int[] a = Tabell.randPerm(20);
-        System.out.println(Arrays.toString(a));
-        Tabell.quicksort(a);
-        System.out.println(Arrays.toString(a));
+    public static void main(String [] args) {
     }
 }
